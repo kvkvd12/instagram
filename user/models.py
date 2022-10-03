e66170e58169595c35969840ae9a3bb4e92c78b6
dy <kvkvd12@gmail.com>
2022-10-03 19:08:23 +0900

    user list 변경

--- user/models.py
+++ user/models.py
@@ -8,7 +8,6 @@ class UserModel(AbstractUser):
     class Meta:
         db_table = 'user'
     
-    email = models.EmailField(verbose_name='email', max_length=100, blank=True, null=True, unique=True)
     bio = models.CharField(max_length=256, default='')
     follow = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followee')
     
